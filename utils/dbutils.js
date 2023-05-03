cf1858fe2b56e78e796e7d6ef78f199d574aea5d
parteekdcrust <pgarg56023@gmail.com>
2023-05-03 18:10:22 +0530

    Error Handling Updated

---
+++ utils/dbutils.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
